41dce35b7434c440a4fa87c907d2a763d7b54bc2
pat-m <patrick@atnos.com>
2018-11-27 09:41:43 +0100

    add pdf invoice and miscs

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,18 +2,46 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/v1/securite/utilisateur")
+ */
 class SecurityController extends AbstractController
 {
     /**
-     * @Route("/security", name="security")
+     * @Route("/connexion", name="security_login", methods="post")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
      */
-    public function index()
+    public function login(Request $request)
     {
-        return $this->render('security/index.html.twig', [
-            'controller_name' => 'SecurityController',
-        ]);
+        $user = new User();
+        return $this->json($user);
+    }
+
+    /**
+     * @Route("/inscription", name="security_register", methods="POST")
+     * @param Request $request
+     * @return Response
+     */
+    public function register(Request $request): Response {
+        var_dump($request->request->get('nom'));
+        $user = new User();
+        $user->setNom($request->request->get('nom'));
+        $user->setPrenom($request->request->get('prenom'));
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+        $form->submit($request->request->all());
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($user);
+        $em->flush();
+
+        return $this->json($user);
     }
 }
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,15 +3,15 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\User2Type;
 use App\Form\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 /**
- * @Route("/utilisateur")
+ * @Route("/v1/utilisateur")
  */
 class UserController extends AbstractController
 {
@@ -28,59 +28,33 @@ class UserController extends AbstractController
     }
 
     /**
-     * @Route("/inscription", name="user_new", methods="POST")
-     */
-    public function new(Request $request, ValidatorInterface $validator): Response
-    {
-        $newUser = json_decode($request->getContent(), true);
-        $user = new User();
-
-        $errors = $validator->validate($user);
-
-
-
-        return $this->json("Votre inscription à bine été prise en compte");
-    }
-
-    /**
      * @Route("/{id}", name="user_show", methods="GET")
      */
     public function show(User $user): Response
     {
-        return $this->json($user);
+        return $this->render('user/show.html.twig', ['user' => $user]);
     }
 
     /**
-     * @Route("/{iduser}/edit", name="user_edit", methods="GET|POST")
+     * @Route("/editer/{id}", name="user_edit", methods="PATCH")
      */
     public function edit(Request $request, User $user): Response
     {
         $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
+        $form->submit($request->request->all());
+        $this->getDoctrine()->getManager()->flush();
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
-
-            return $this->redirectToRoute('user_index', ['iduser' => $user->getIduser()]);
-        }
-
-        return $this->render('user/edit.html.twig', [
-            'user' => $user,
-            'form' => $form->createView(),
-        ]);
+        return $this->json($user);
     }
 
     /**
-     * @Route("/{iduser}", name="user_delete", methods="DELETE")
+     * @Route("/supprimer/{id}", name="user_delete", methods="DELETE")
      */
     public function delete(Request $request, User $user): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$user->getIduser(), $request->request->get('_token'))) {
-            $em = $this->getDoctrine()->getManager();
-            $em->remove($user);
-            $em->flush();
-        }
-
-        return $this->redirectToRoute('user_index');
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($user);
+        $em->flush();
     }
 }
